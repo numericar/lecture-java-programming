d2557abbe32a8e7b3629137ac5e4e85a58467029
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-13 09:41:37 +0700

    initial arithmetic project

--- DataSizeRange.java
+++
@@ -1,5 +0,0 @@
-public class DataSizeRange {
-    public static void main(String[] arg) {
-        System.out.println(Integer.MAX_VALUE);
-    }
-}
--- Variable.java
+++
@@ -1,7 +0,0 @@
-public class Variable {
-    public static void main(String[] args) {
-        byte b;
-        b = 5;
-        System.out.println(b);
-    }
-}

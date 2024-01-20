2db90c951ee0db48f7dbcdef4d1504c04f35a82c
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-20 11:12:42 +0700

    lecture - Bitwise

--- codes/CodeLecture/src/Bitwise/BitwiseDemo.java
+++ codes/CodeLecture/src/Bitwise/BitwiseDemo.java
@@ -2,6 +2,11 @@ package Bitwise;
 
 public class BitwiseDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int x = 10, y = 6;
+        System.out.println(x & y);
+        System.out.println(x | y);
+        System.out.println(x ^ y);
+        System.out.println(x << 1);
+        System.out.println(x >> 1);
     }
 }

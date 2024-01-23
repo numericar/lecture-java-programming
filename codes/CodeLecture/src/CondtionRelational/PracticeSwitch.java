24946793d5c4405c98c5e8a5208e02762746a639
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-23 09:38:33 +0700

    lecture - switch case

---
+++ codes/CodeLecture/src/CondtionRelational/PracticeSwitch.java
@@ -0,0 +1,18 @@
+package CondtionRelational;
+
+public class PracticeSwitch {
+    public static void main(String[] args) {
+        int number = 8;
+
+        switch (number) {
+            case 5:
+                System.out.println("Successful");
+                break;
+            case 6:
+                System.out.println("Success");
+                break;
+            default:
+                System.out.println("Not found");
+        }
+    }
+}

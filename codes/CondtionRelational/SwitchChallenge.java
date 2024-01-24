c6763a60c1ca7311b5c634377e4835a70de78ef9
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-24 08:47:37 +0700

    lecture - switch challenge

---
+++ codes/CondtionRelational/SwitchChallenge.java
@@ -0,0 +1,38 @@
+package CondtionRelational;
+
+import java.util.Scanner;
+
+public class SwitchChallenge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter day number: ");
+        int day = sc.nextInt();
+        sc.close();
+
+        switch (day) {
+            case 1:
+                System.out.println("Mon");
+                break;
+            case 2:
+                System.out.println("Tue");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thur");
+                break;
+            case 5:
+                System.out.println("Fri");
+                break;
+            case 6:
+                System.out.println("Sat");
+                break;
+            case 7:
+                System.out.println("Sun");
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+    }
+}

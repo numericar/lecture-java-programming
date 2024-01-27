3eb2b2f3f899bc90cf5cae2fae8bf9c0b478c490
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-27 17:10:59 +0700

    lecture - Passing object as parameters

---
+++ codes/Method/MethodPractic.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class MethodPractic {
+    public static void main(String[] args) {
+        System.out.println(max(10, 20));
+    }
+
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+}

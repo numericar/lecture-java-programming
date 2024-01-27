8fb589270ad8bdcfec9edb5b63c0fed320adc0f7
Colorfuls <mint.rosetta2001@gmail.com>
2024-01-27 09:21:22 +0700

    lecture - nested loop

---
+++ codes/Loops/NestedLoops.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class NestedLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("(" + i + "," + j + ")\t");
+            }
+            System.out.println();
+        }
+    }
+}

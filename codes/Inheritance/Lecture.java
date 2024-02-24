ae7c7900ca58f09eb481e84e242141c71c098ea3
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-24 12:48:43 +0700

    lecture - practice for inheritance

---
+++ codes/Inheritance/Lecture.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Lecture {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder();
+        cylinder.radius = 7;
+        cylinder.height = 10;
+
+        System.out.println("Valume " + cylinder.volume());
+        System.out.println("Area " + cylinder.area());
+    }
+}

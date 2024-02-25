1b612565ca3cb2c6645255c3d68b713714947100
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-25 09:21:01 +0700

    lecture - this vs super

---
+++ codes/Inheritance/ThisSuper.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class ThisSuper {
+    int length;
+    int breadth;
+
+    public ThisSuper(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    void display() {
+        System.out.println("Length : " + this.length);
+        System.out.println("Breadth : " + this.breadth);
+    }
+}

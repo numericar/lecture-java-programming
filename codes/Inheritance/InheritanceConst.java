e7384cdaa70cfbd2c025d2956f2f3a0759046c9b
Colorfuls <mint.rosetta2001@gmail.com>
2024-02-25 08:24:04 +0700

    lecture - parameter of constructor in inheritance

--- codes/Inheritance/InheritanceConst.java
+++ codes/Inheritance/InheritanceConst.java
@@ -2,7 +2,7 @@ package Inheritance;
 
 public class InheritanceConst {
     public static void main(String[] args) {
-        GrandChild c = new GrandChild();
+        Child c = new Child(4);
     }
 }
 
@@ -10,16 +10,28 @@ class Parent {
     public Parent() {
         System.out.println("Parent Constructor");
     }
+
+    public Parent(int x) {
+        System.out.println("Param of Parent Constructor");
+    }
 }
 
 class Child extends Parent {
     public Child() {
         System.out.println("Child Constructor");
     }
+
+    public Child(int x) {
+        super(4);
+        System.out.println("Param of Child Constructor");
+    }
 }
 
 class GrandChild extends Child {
     public GrandChild() {
         System.out.println("GrandChild Constructor");
+    }
+    public GrandChild(int x) {
+        System.out.println("Param of GrandChild Constructor");
     }
 }

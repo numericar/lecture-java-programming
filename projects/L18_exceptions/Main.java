afc46fcf55edf2c932735b3051a86d51049412fd
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-13 22:08:16 +0700

    lecture - exception

--- projects/L18_exceptions/Main.java
+++ projects/L18_exceptions/Main.java
@@ -2,15 +2,6 @@ package L18_exceptions;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            int number = Integer.parseInt("s");
-            System.out.println(number);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        } finally {
-            System.out.println("Finally");
-        }
-
-        System.out.println("End");
+        System.out.println("Hello World");
     }
 }

593d8ac14574ca4527f2c8bbd999bbdab6392a02
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-12 11:52:09 +0700

    lecture - math

---
+++ projects/L19_langpackage/MathDemo.java
@@ -0,0 +1,10 @@
+package L19_langpackage;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println(Math.getExponent(12.34));
+        System.out.println(50f/9f);
+        System.out.println(Math.floorDiv(50, 9));
+        System.out.println(Math.random() * 10);
+    }
+}

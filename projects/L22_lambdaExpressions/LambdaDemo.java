93191856b19ee194959d90f88c7d72b4f11e1d94
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-13 13:45:23 +0700

    expression with paramter and return

--- projects/L22_lambdaExpressions/LambdaDemo.java
+++ projects/L22_lambdaExpressions/LambdaDemo.java
@@ -3,24 +3,33 @@ package L22_lambdaExpressions;
 public class LambdaDemo {
     public static void main(String[] args) {
         // () -> {} เป็นการประกาศ lambda expression โดย interface จะต้องเป็น FunctionalInterface เท่านั้น
-        MyLamda a = () -> {
-            System.out.println("Hello Lambda");
+        // ไม่ต้องระบุ parameter type เพราะว่า complier จะรู้อยู่แล้วโดยอ้างอิงกับ interface, แต่สามารถระบุได้
+        MyLamda a = (message) -> {
+            System.out.println(message);
         };
+        a.display("Hello World");
 
-        a.display();
+        // ถ้าหากมีการ return ค่า เราสามารถเรียกใช้งานโดยไม่ต้องระบุ block scope ได้
+        MuLambda2 b = (int n1, int n2) -> n1 + n2;
+        System.out.println(b.add(10, 10));
     }
 }
 
 // ถ้าหาก interface มีเพียงการทำงานแค่ method เดียว เราจะเรียกว่า FunctionalInterface
 @FunctionalInterface
 interface MyLamda {
-    public void display();
+    public void display(String message);
+}
+
+@FunctionalInterface
+interface MuLambda2 {
+    public int add(int a, int b);
 }
 
 class My implements MyLamda {
 
     @Override
-    public void display() {
+    public void display(String message) {
        System.out.println("Hello World");
     }
 
